a06eb81f543577189eeb04532bf90a08b7fd0dfa
JordanHimawan003 <jordan.hiphop21@gmail.com>
2021-11-25 19:42:02 +0700

    Variabel

---
+++ Eps 04 - Mengenal Variabel/main.py
@@ -0,0 +1,29 @@
+# variabel adalah tempat menyimpan data
+
+# assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# contoh penamaan variabel
+nilai_y = 15 #menggunakan underscore
+juta10 = 10000000 #ini boleh
+nilaiZ = 17.5 #camel case
+
+#pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
+
+
+
+#25 Nov 2021 - Python untuk persiapan LKS
