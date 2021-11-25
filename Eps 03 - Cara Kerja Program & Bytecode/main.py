fdcc11c3fb86a80daa1b9b3e1cdbc4ec84d785dc
JordanHimawan003 <jordan.hiphop21@gmail.com>
2021-11-25 19:24:05 +0700

    Alur Program PY

--- Eps 03 - Cara Kerja Program & Bytecode/main.py
+++ Eps 03 - Cara Kerja Program & Bytecode/main.py
@@ -1,1 +1,18 @@
-print("Hellow world")
+print("Hello")
+print("World")
+print("Hello World")
+print("Python mengeksekusi berurutan")
+#python mengeksekusi kode berdasarkan urutan dari atas ke bawah
+        #baris kosong tidak akan di eksekusi
+#ini adalah comment
+""" ini adalah multiline
+comment
+"""
+a = 10
+print(a) #jika print a diletakkan sebelum a=10 maka akan terjadi error
+
+#kita bisa mengcompile python ke bytecode
+#buka terminal, arahkan ke direktori main.py, ketikkan :
+#python -m py_compile main.py
+
+#25 Nov 2021 - Python untuk persiapan LKS
